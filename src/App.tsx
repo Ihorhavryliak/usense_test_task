666f36579a1d8a2528527c7a1c3600d40508b8f8
Ihor <kachkamala@gmail.com>
2023-03-30 19:55:39 +0300

    delete symbols

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+
+  const [passwordStrength, setPasswordStrength] = useState("")
+  const [color, setColor] = useState("")
+  const [lengthField, setLengthField] = useState("")
+
+  const onChangeField = (text: string) => {
+    setLengthField(text)
+    //check
+    const isLetters = /[A-Z]/gi.test(text)
+    const isDigits = /[0-9]/g.test(text)
+    const isSymbols = /[!@#$%^&*(),.?":{}|<>]/g.test(text)
+
+    if (text.length === 0) {
+      setPasswordStrength("")
+    } else if (isSymbols && !isDigits && !isLetters) {
+      setPasswordStrength("easy")
+    } else if (!isSymbols && isDigits && !isLetters) {
+      setPasswordStrength("easy")
+    } else if (!isSymbols && !isDigits && isLetters) {
+      setPasswordStrength("easy")
+    } else if (isSymbols && !isDigits && isLetters) {
+      setPasswordStrength("medium")
+    } else if (!isSymbols && isDigits && isLetters) {
+      setPasswordStrength("medium")
+    } else if (isSymbols && isDigits && !isLetters) {
+      setPasswordStrength("medium")
+    } else {
+      setPasswordStrength("strong")
+    }
+
+    if (text.length === 0) {
+      setColor("gray")
+    } else if (text.length < 8) {
+      setColor("red")
+    } else {
+      setColor("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div>
+        <div className="mb-2">
+          <input type="text" onChange={(e) => onChangeField(e.target.value)} />
+        </div>
+        <div className="mb-2">
+          <div
+            className={
+              color === "gray"
+                ? "gray"
+                : color === "red"
+                ? "red"
+                : passwordStrength === "easy"
+                ? "red"
+                : passwordStrength === "strong"
+                ? "green"
+                : passwordStrength === "medium"
+                ? "yellow"
+                : color
+            }
+          >
+            the password is easy
+          </div>
+          <div
+            className={
+              color === "gray"
+                ? "gray"
+                : color === "red"
+                ? "red"
+                : passwordStrength === "easy"
+                ? "gray"
+                : passwordStrength === "strong"
+                ? "green"
+                : passwordStrength === "medium"
+                ? "yellow"
+                : color
+            }
+          >
+            the password is medium
+          </div>
+          <div
+            className={
+              color === "gray"
+                ? "gray"
+                : color === "red"
+                ? "red"
+                : passwordStrength === "easy"
+                ? "gray"
+                : passwordStrength === "strong"
+                ? "green"
+                : passwordStrength === "medium"
+                ? "gray"
+                : color
+            }
+          >
+            the password is strong
+          </div>
+        </div>
+        <div>Password is: {passwordStrength}</div>
+        <div>Length is: {lengthField.length}</div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
